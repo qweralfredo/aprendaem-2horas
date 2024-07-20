954769654318c0496bf8a102deaa773aa68a57f5
qweralfredo <qwer.alfredo@gmail.com>
2024-07-20 02:30:00 +0000

    sql de criação

--- lord-duckdb/data/aula5/warehouse.sql
+++ lord-duckdb/data/aula5/warehouse.sql
@@ -1,61 +1,110 @@
-CREATE TABLE Fato_Ocorrencia (
-	codigo_ocorrencia INT PRIMARY KEY,
-	ocorrencia_dia DATE,
-	ocorrencia_hora TIME,
-	ocorrencia_tipo VARCHAR(255),
-	ocorrencia_cidade VARCHAR(255),
-	ocorrencia_uf CHAR(2),
-	ocorrencia_pais VARCHAR(255),
-	ocorrencia_aerodromo VARCHAR(4),
-	ocorrencia_saida_pista varchar(3),
-	total_recomendacoes INT,
-	total_aeronaves_envolvidas INT,
-	ocorrencia_fatalidades_total INT
-);
+select * from ocorrencia;
 
-CREATE TABLE Fato_Fator_Contribuinte (
-	codigo_ocorrencia INT,
-	fator_nome VARCHAR(255),
-	fator_aspecto VARCHAR(255),
-	fator_condicionante BOOLEAN,
-	fator_area VARCHAR(255),
-	PRIMARY KEY (codigo_ocorrencia, fator_nome)
-);
+select * from fator_contribuinte fc ;
 
-CREATE TABLE Fato_Investigacao (
-	codigo_ocorrencia INT PRIMARY KEY,
-	investigacao_aeronave_liberada BOOLEAN,
-	investigacao_status VARCHAR(255)
-);
+select * from aeronave a ;
 
-CREATE TABLE Fato_Divulgacao (
-	codigo_ocorrencia INT PRIMARY KEY,
-	divulgacao_relatorio_numero VARCHAR(255),
-	divulgacao_relatorio_publicado BOOLEAN,
-	divulgacao_dia_publicacao DATE
-);
+select * from ocorrencia_tipo ot  ;
 
-CREATE TABLE Dimensao_Aeronave (
-	codigo_aeronave INT PRIMARY KEY,
-	aeronave_matricula VARCHAR(255),
-	aeronave_tipo_veiculo VARCHAR(255),
-	aeronave_fabricante VARCHAR(255),
-	aeronave_modelo VARCHAR(255),
-	aeronave_tipo_cao VARCHAR(255),
-	aeronave_motor_tipo VARCHAR(255),
-	aeronave_motor_quantidade INT,
-	aeronave_pmd BOOLEAN,
-	aeronave_pmd_categoria VARCHAR(255),
-	aeronave_assentos INT,
-	aeronave_ano_fabricacao int,
-	aeronave_pais_fabricante VARCHAR(255),
-	aeronave_pais_registro VARCHAR(255),
-	aeronave_registro_categoria VARCHAR(255),
-	aeronave_registro_segmento VARCHAR(255),
-	aeronave_voo_origem VARCHAR(255),
-	aeronave_voo_destino VARCHAR(255),
-	aeronave_fase_operacao VARCHAR(255),
-	aeronave_tipo_operacao VARCHAR(255)
-);
+select * from recomendacao r ;
+
+
+ 
+ 
+select 
+o.codigo_ocorrencia,
+DENSE_RANK()  over ( order by ot.ocorrencia_tipo asc ) as codigo_ocorrencia_tipo,
+ot.ocorrencia_tipo ,
+DENSE_RANK()  over ( order by ot.ocorrencia_tipo_categoria,ot.taxonomia_tipo_icao  asc ) as codigo_ocorrencia_tipo_categoria,
+ot.ocorrencia_tipo_categoria,
+ot.taxonomia_tipo_icao,
+dense_rank () over (order by o.ocorrencia_uf) as codigo_uf,
+o.ocorrencia_uf ,
+dense_rank () over (order by o.ocorrencia_cidade) as codigo_cidade,
+o.ocorrencia_cidade,
+dense_rank () over (order by o.ocorrencia_pais) as codigo_pais,
+o.ocorrencia_pais,
+dense_rank () over (order by o.ocorrencia_aerodromo) as codigo_aerodromo,
+o.ocorrencia_aerodromo,
+o.ocorrencia_dia,
+o.ocorrencia_hora, 
+o.ocorrencia_cidade,
+o.ocorrencia_uf,
+o.ocorrencia_pais,
+o.ocorrencia_aerodromo,
+o.ocorrencia_saida_pista
+from ocorrencia o 
+left join ocorrencia_tipo ot on o.codigo_ocorrencia1 = ot.codigo_ocorrencia1
+left join aeronave a on o.codigo_ocorrencia2 = a.codigo_ocorrencia2
+left join fator_contribuinte fc on o.codigo_ocorrencia3 = fc.codigo_ocorrencia3
+left join recomendacao r on o.codigo_ocorrencia4 = r.codigo_ocorrencia4  
+
+
+
+
+
+
+--fato
+	--calendario
+		--hora
+		--minuto
+		--dia
+		--mes
+		--ano
+	--ocorrencia
+		
+	
+
+--dimensao	
+	--ocorrencia
+		--classificacao
+		--cidade
+		--uf
+		--pais
+		--aerodromo
+		--investigacao_status
+		--investigacao_aeronave_liberada
+		--divulgacao_relatorio_publicado
+		--ocorrencia_saida_pista
+
+	--ocorrencia_tipo (ocorrencia_tipo.ocorrencia_tipo, taxonomia_tipo_icao)
+	--ocorrencia_categoria (ocorrencia_tipo.ocorrencia_categoria, taxonomia_tipo_icao)
+
+	--aeronave
+		--tipo_veiculo
+		--fabricante
+		--modelo
+		--tipo_icao
+		--motor_tipo
+		--motor_quantidade
+		--pmd
+		--assentos
+		--ano_fabricacao
+		--pais_fabricante
+		--pais_registro
+		--registro_categoria
+		--registro_segmento
+		--voo_origem
+		--voo_destino
+		--fase_operacao
+		--tipo_operacao
+		--nivel_dano
+	--fator
+		--fator_contribuinte
+		--fator_aspecto
+		--fator_condicionamento
+		--fator_area
+		
+		
+	--recomendacao_destino (recomendacao.recomendacao_destinatario, recomendacao.recomendacao_conteudo)
+	--recomendacao_sigla (recomendacao.recomendacao_sigla)
+
+
+
+--criar chaves nas dimensoes, e atualizar os fatos com as referencias
+--trocar valores nulos com N/A ou Unknow 
+
+
+	
 
 
